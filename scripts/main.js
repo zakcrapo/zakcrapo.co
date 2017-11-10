a8bb874a29fa085713c81c702f6a619eeb7852fb
Zak Crapo <hey@zakcrapo.co>
2017-11-10 15:53:37 -0600

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// var myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello World!';
+
+// $('#btnClick').on('click',function(){
+//     if($('#1').css('display')!='none'){
+//     $('#2').html('Here is my dynamic content').show().siblings('div').hide();
+//     }else if($('#2').css('display')!='none'){
+//         $('#1').show().siblings('div').hide();
+//     }
+// });
